587822c76ad2834d62ccd90cdcb7ed7820857721
Nima Ghasemi Por <71447191+nimagp@users.noreply.github.com>
2021-11-18 22:57:50 +0330

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from googlesearch import search
 from bs4 import BeautifulSoup
 import urllib
+#a comment
 #Create googletrans instance
 path = Path ("").parent.absolute()
 translator = Translator()
